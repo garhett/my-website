31e9f6ad649b1838ab182a084e911bc815866094
garhett <60421166+garhett@users.noreply.github.com>
2022-02-17 20:53:02 -0700

    Initial Push

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      //      <img src="hello-there.jpg" alt="Hello There">
-      <p>Hello There</p>
     </div>
   );
 }
